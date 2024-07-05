b3cfa4b9e35159acca623d38a1e248f6ec43190b
novinbukannopin <novinardiany@outlook.com>
2024-07-05 23:56:41 +0700

    Keep it Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,53 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { zValidator } from "@hono/zod-validator";
 import { LoginSchema } from "./schema/login";
 import { HTTPException } from "hono/http-exception";
-import { sign } from "hono/jwt"
-import { setCookie, getCookie } from "hono/cookie"
-import {bearerAuth} from "hono/bearer-auth";
+import { sign } from "hono/jwt";
+import { getCookie, setCookie } from "hono/cookie";
+import { bearerAuth } from "hono/bearer-auth";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use(logger())
+app.use(logger());
 
-app.post('/login', zValidator('json', LoginSchema),  async (c) => {
-  const { email, password } = await c.req.json()
+app.post("/login", zValidator("json", LoginSchema), async (c) => {
+  const { email, password } = await c.req.json();
 
-  if (password !== 'password') {
-    throw new HTTPException(401, { message: "Invalid credentials" })
+  if (password !== "password") {
+    throw new HTTPException(401, { message: "Invalid credentials" });
   }
 
   const payload = {
     email,
-    exp: Math.floor(Date.now() / 1000) + 60 * 60
-  }
+    exp: Math.floor(Date.now() / 1000) + 60 * 60,
+  };
 
-  const token = await sign(payload, Bun.env.JWT_SECRET || '')
-  setCookie(c, 'token', token)
+  const token = await sign(payload, Bun.env.JWT_SECRET || "");
+  setCookie(c, "token", token);
   return c.json({
     payload,
-    token
-  })
-})
+    token,
+  });
+});
 
-app.use('/index/*', bearerAuth({
-  verifyToken: async (token, c) => {
-    return token === getCookie(c, 'token')
-  }
-}))
+app.use(
+  "/index/*",
+  bearerAuth({
+    verifyToken: async (token, c) => {
+      return token === getCookie(c, "token");
+    },
+  }),
+);
 
-app.get('/index/movies', (c) => {
+app.get("/index/movies", (c) => {
   return c.json({
     movies: [
-      { title: 'Inception', year: 2010 },
-      { title: 'Interstellar', year: 2014 },
-      { title: 'Dunkirk', year: 2017 },
-    ]
-  })
-})
+      { title: "Inception", year: 2010 },
+      { title: "Interstellar", year: 2014 },
+      { title: "Dunkirk", year: 2017 },
+    ],
+  });
+});
 
-export default app
+export default app;
--- src/schema/login.ts
+++ src/schema/login.ts
@@ -1,6 +1,6 @@
-import { z } from "zod"
+import { z } from "zod";
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
-})
+  email: z.string().email(),
+  password: z.string().min(8),
+});
